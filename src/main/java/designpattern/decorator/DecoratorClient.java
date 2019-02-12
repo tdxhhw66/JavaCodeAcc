a420b031f5645390b09263c4873a7014743e36ee
tdxhhw66 <tdxhhw66@gmail.com>
2019-02-13 00:03:25 +0800

    添加文件

--- src/main/java/designpattern/decorator/DecoratorClient.java
+++ src/main/java/designpattern/decorator/DecoratorClient.java
@@ -1,5 +1,14 @@
 package designpattern.decorator;
 
+import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 /**
  * 装饰模式客户端调用代码，装饰的过程更像是层层包装，用前面的对象装饰后面的对象
  * 
@@ -7,16 +16,19 @@ package designpattern.decorator;
  *
  */
 public class DecoratorClient {
-    public static void main(String[] args) {
-	ConcreteComponent concreteComponent = new ConcreteComponent();
-	ConcreteDecoratorA concreteDecoratorA = new ConcreteDecoratorA();
-	ConcreteDecoratorB concreteDecoratorB = new ConcreteDecoratorB();
-	ConcreteDecoratorC concreteDecoratorC = new ConcreteDecoratorC();
+	public static void main(String[] args) throws FileNotFoundException {
+		ConcreteComponent concreteComponent = new ConcreteComponent();
+		ConcreteDecoratorA concreteDecoratorA = new ConcreteDecoratorA();
+		ConcreteDecoratorB concreteDecoratorB = new ConcreteDecoratorB();
+		ConcreteDecoratorC concreteDecoratorC = new ConcreteDecoratorC();
 
-	concreteDecoratorA.setComponent(concreteComponent);
-	concreteDecoratorB.setComponent(concreteDecoratorA);
-	concreteDecoratorC.setComponent(concreteDecoratorB);
-	concreteDecoratorC.operation();
+		concreteDecoratorA.setComponent(concreteComponent);
+		concreteDecoratorB.setComponent(concreteDecoratorA);
+		concreteDecoratorC.setComponent(concreteDecoratorB);
+		concreteDecoratorC.operation();
 
-    }
+		InputStream inputStream = new FileInputStream("test.txt");
+		InputStream bufferedInputStream = new BufferedInputStream(inputStream);
+		
+	}
 }
